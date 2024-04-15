f545b088558893d412b4b606e79da22ae569803b
Oto Zarins <58180186+imafakester@users.noreply.github.com>
2024-04-15 15:39:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert(1);
+//console.log(1);
+function sutitZinu()
+{
+    console.log('sutitZinu()Darbojas');
+    let zina= document.querySelector('zina').value;
+    let zinas= document.querySelector('cataZina').value;
+    zinas.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
+}
