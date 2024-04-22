e2765631f0dd0b8d4817dd6f5cd54053a46e2c69
Oto Zarins <58180186+imafakester@users.noreply.github.com>
2024-04-22 15:36:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ function sutitZinu()
     console.log('sutitZinu()darbojas');
     zinas.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
 }
-function ieladetChataZinas()
+async function ieladetChataZinas()
 {
     let datiNoServera = await fetch('chatazinas.txt');
     let dati = await datiNoServera.text;
+    console.log(dati)
 }
